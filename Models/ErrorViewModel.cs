7bb8aca69459b6627db6d692064fdc76d6ebef1c
Rivan jaradat <rivanr44@gmail.com>
2025-04-02 23:23:25 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_eCommerce_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
