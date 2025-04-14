2af9435eabdca2219970b7abca40c2228354df3a
Rivan jaradat <rivanr44@gmail.com>
2025-04-14 19:07:44 +0300

    Products Page & Product Details Page

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_eCommerce_project.Data;
+using System.Security.Policy;
+
+namespace MVC_eCommerce_project.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context; // Change to ApplicationDbContext
+
+        public ProductsController(ApplicationDbContext context) // Change to ApplicationDbContext
+        {
+          
+            this.context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var product = await context.Products.ToListAsync();
+            return View(product);
+        }
+        public async Task< IActionResult> Details(int id)
+        {
+            var product = await context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
