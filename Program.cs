d8bec216d8b8f7c6295fdb4a28598879d5a4bcd8
Rivan jaradat <rivanr44@gmail.com>
2025-08-15 18:39:21 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ namespace MVC_eCommerce_project
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            
                 app.UseHsts();
             }
 
